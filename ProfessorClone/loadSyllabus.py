c5af0205eb0f20d4b80789b50ec1e24dc0b12e01
Bryan Erickson <spyro3277@gmail.com>
2026-01-19 16:01:47 -0600

    Moved homepage to it's own folder

--- ProfessorClone/loadSyllabus.py
+++ ProfessorClone/loadSyllabus.py
@@ -24,24 +24,6 @@ while True:
         break
 
 
-
-#Prints all the top-level outlines in a pdf
-topLevelOutlines = []
-#print("Top-level outlines:")
-#print("-" * 32)
-
-for outline in reader.outline:
-    if isinstance(outline, list):
-        continue
-    page_number = reader.get_destination_page_number(outline)
-
-    if page_number is None:
-        #print(f"{outline.title} -> No page destination")
-        topLevelOutlines.append(outline.title)
-    else:
-        #print(f"{outline.title} -> page {page_number + 1}")
-        topLevelOutlines.append(outline.title)
-
 #Prints the nested outlines
 nestedOutlines = []
 def print_outline(
@@ -102,6 +84,7 @@ print(data)
 #Clears the created txt file 
 with open(outfile, 'w') as file:
     pass
+
 
 
 
